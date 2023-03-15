249c698a21e67066e8e9af83af0a8622119281ba
Thaisa Alice <thaisalic3@icloud.com>
2023-03-14 23:31:22 -0300

    feat: structure and style of all pages implementing

--- static/script.js
+++ static/script.js
@@ -4,8 +4,8 @@ const body = document.querySelector('body')
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.contains('bi-list')
-    ? menuMobile.classList.replace('bi-list', 'bi-x')
-    : menuMobile.classList.replace('bi-x", "bi-list')
+        ? menuMobile.classList.replace('bi-list', 'bi-x')
+        : menuMobile.classList.replace('bi-x', 'bi-list')
 
     body.classList.toggle('menu-nav-active')
 })
@@ -21,3 +21,40 @@ navItem.forEach((item) => {
         }
     })
 })
+// Animar todos os itens na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
+
+// Ativar carregamento no botão de enviar formulário para
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", () => {
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
+})
+
+// Tira a mensagem de sucesso depois de 5 segundos
+
+setTimeout(() => {
+    document.querySelector('#alerta').style.display = 'none';
+}, 5000)
