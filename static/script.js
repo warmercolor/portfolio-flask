4a751a355d969432f21549772b43215a197e5894
Thaisa Alice <thaisalic3@icloud.com>
2023-03-15 01:00:44 -0300

    feat: debug in progress

--- static/script.js
+++ static/script.js
@@ -23,23 +23,23 @@ navItem.forEach((item) => {
 })
 // Animar todos os itens na tela que tiverem meu atributo data-anime
 
-const item = document.querySelectorAll("[data-anime]");
+const item = document.querySelectorAll('[data-anime]');
 
 const animeScroll = () => {
     const windowTop = window.pageYOffset + window.innerHeight * 0.85;
 
     item.forEach((element) => {
         if (windowTop > element.offsetTop) {
-            element.classList.add("animate");
+            element.classList.add('animate');
         } else {
-            element.classList.remove("animate");
+            element.classList.remove('animate');
         }
     });
 };
 
 animeScroll();
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
     animeScroll();
 })
 
@@ -48,9 +48,9 @@ window.addEventListener("scroll", () => {
 const btnEnviar = document.querySelector('#btn-enviar')
 const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 
-btnEnviar.addEventListener("click", () => {
-    btnEnviarLoader.style.display = "block";
-    btnEnviar.style.display = "none"
+btnEnviar.addEventListener('click', () => {
+    btnEnviarLoader.style.display = 'block';
+    btnEnviar.style.display = 'none'
 })
 
 // Tira a mensagem de sucesso depois de 5 segundos
